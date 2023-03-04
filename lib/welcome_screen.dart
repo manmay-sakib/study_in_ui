653a5a5c262cb1cc4be1ca1c600300255de5e4aa
manmay-sakib <monmy.sakib@gmail.com>
2023-03-04 13:22:56 +0600

    refactored. Moved welcome screen code to new file

---
+++ lib/welcome_screen.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            flex: 3,
+            child: ClipPath(
+              clipper: PathClipper(),
+              child: ColoredBox(
+                color: Colors.black,
+                child: SizedBox.expand(
+                  child: Padding(
+                    padding: const EdgeInsets.all(24.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Expanded(
+                          flex: 17,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Text(
+                                "Welcome to",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w300,
+                                  fontSize: 32,
+                                ),
+                              ),
+                              // RichText(
+                              //   text: TextSpan(
+                              //     children: [
+                              //       TextSpan(
+                              //         text: 'study',
+                              //       ),
+                              //       TextSpan(
+                              //         text: 'IN',
+                              //       )
+                              //     ]
+                              //   ),
+                              // )
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    'study ',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 64,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                  Text(
+                                    'IN',
+                                    style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 64,
+                                      fontWeight: FontWeight.w300,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                        Spacer(flex: 3)
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: Column(
+              children: [
+                Spacer(),
+                Text(
+                  'Let\'s Begin Your',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontSize: 32,
+                  ),
+                ),
+                SizedBox(height: 16),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Journey",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 32,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    backgroundColor: Colors.black,
+                    minimumSize: Size(0, 72),
+                    padding: EdgeInsets.symmetric(
+                      horizontal: 48,
+                    ),
+                  ),
+                ),
+                Spacer(),
+                TextButton.icon(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.arrow_back,
+                    color: Colors.black,
+                    size: 24,
+                  ),
+                  label: Text(
+                    'back',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 24,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class PathClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    double x = size.width;
+    double y = size.height;
+    path.moveTo(0, 0);
+    path.lineTo(0, y * 0.85);
+    path.quadraticBezierTo(x / 2, y * 1.15, x, y * 0.85);
+    path.lineTo(x, 0);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return true;
+  }
+}
